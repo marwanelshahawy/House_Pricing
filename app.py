f50027b0ca52663acd256ae3ff953fca5d88d555
“[Marwan <marawanwaleed27@gmail.com>
2026-08-03 01:48:48 +0300

    Web App

---
+++ app.py
@@ -0,0 +1,32 @@
+import pickle
+import json
+from flask import Flask, request, jsonify, app, url_for, render_template
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+app = Flask(__name__)
+# Load the trained model
+model = pickle.load(open('MLmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0][0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
