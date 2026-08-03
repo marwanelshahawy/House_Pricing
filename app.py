8fc3462747b2a3d40b9edcba2c2c8826f4291f00
“[Marwan <marawanwaleed27@gmail.com>
2026-08-03 03:34:07 +0300

    web app finished

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
     print(data)
@@ -26,6 +26,12 @@ def predict_api():
     print(output[0])
     return jsonify(output[0][0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = model.predict(final_input)[0][0]
+    return render_template("index.html", prediction_text="The House Price is ${:.2f}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
